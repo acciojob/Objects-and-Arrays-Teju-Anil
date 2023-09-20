5dd77157b422097e1892176a1cdda0d91a42130d
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-09-20 07:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+const team = players;
 
+// Create a copy of players
+const team1 = players.slice();
+
+// Create a copy of person
+const cap1 = Object.assign({}, person);
+
+// Test if the variables have been created correctly
+console.log(team);
+console.log(team1);
+console.log(cap1);
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
